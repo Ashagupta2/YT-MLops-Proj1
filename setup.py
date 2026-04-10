5864f157a2e97bd4cc400e47fa1b308de765779d
Asha <guptaaasha2618@gmail.com>
2026-04-10 17:35:34 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Asha Gupta",
+    author_email="guptaasha2618@gmail.com",
+    packages=find_packages()
+)
